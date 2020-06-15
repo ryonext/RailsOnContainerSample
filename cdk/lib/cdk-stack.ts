ea53494867d9c5c0a1f89b58ffbb143b1b28f9a2
ryonext <ryonext@gmail.com>
2020-06-15 23:36:39 +0900

    Create cluster.

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -3,10 +3,23 @@ import * as s3 from '@aws-cdk/aws-s3';
 import * as codepipeline from '@aws-cdk/aws-codepipeline';
 import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
 import * as codebuild from '@aws-cdk/aws-codebuild';
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as ecs from "@aws-cdk/aws-ecs";
+import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
+
 
 export class CdkStack extends core.Stack {
   constructor(scope: core.Construct, id: string, props?: core.StackProps) {
     super(scope, id, props);
+
+
+    const vpc = new ec2.Vpc(this, "MyVpc", {
+      maxAzs: 3 // Default is all AZs in region
+    });
+
+    const cluster = new ecs.Cluster(this, "MyCluster", {
+      vpc: vpc
+    });
 
     // The code that defines your stack goes here
     const targetBucket = new s3.Bucket(this, 'MyFirstBucket', {
